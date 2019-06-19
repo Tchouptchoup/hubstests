c938b447a92e7f9474d69bd8895d318a9066dbc9
Tchouptchoup <maitetxu.darguy@gmail.com>
2019-06-19 10:39:10 +0200

    Player + Table des titres

--- src/components/music/Music.js
+++ src/components/music/Music.js
@@ -1,8 +1,36 @@
 import React, { Component } from "react";
+import { Table } from "semantic-ui-react";
 
 class Music extends Component {
   render() {
-    return <div>"Music"</div>;
+    return (
+      <div>
+        <figure>
+          <figcaption>Listen to the T-Rex:</figcaption>
+          <audio controls src="/media/examples/t-rex-roar.mp3">
+            Your browser does not support the
+            <code>audio</code> element.
+          </audio>
+        </figure>
+        <Table basic="very" celled collapsing>
+          <Table.Header>
+            <Table.Row>
+              <Table.HeaderCell>Music Title</Table.HeaderCell>
+              <Table.HeaderCell>Play</Table.HeaderCell>
+              <Table.HeaderCell>Add to my playlist</Table.HeaderCell>
+            </Table.Row>
+          </Table.Header>
+
+          <Table.Body>
+            <Table.Row>
+              <Table.Cell>Titre</Table.Cell>
+              <Table.Cell>Play</Table.Cell>
+              <Table.Cell>Add</Table.Cell>
+            </Table.Row>
+          </Table.Body>
+        </Table>
+      </div>
+    );
   }
 }
 
