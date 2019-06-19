3da98d5a64321b173b24752a16d831dd2adf6bd8
Tchouptchoup <maitetxu.darguy@gmail.com>
2019-06-19 10:28:42 +0200

    router OK + installation de semantic ui et mise en place du menu

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { Component } from "react";
 import "./App.css";
+import { Switch, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Music from "./components/music/Music";
+import Prime from "./components/prime/Prime";
+import Graph from "./components/graph/Graph";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Navbar />
+        <Switch>
+          <Route exact path="/music" component={Music} />
+          <Route path="/prime" component={Prime} />
+          <Route path="/graph" component={Graph} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
