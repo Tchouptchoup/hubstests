7c5e7bf192a4d4bf7f41da078aed72b8373145f0
Tchouptchoup <maitetxu.darguy@gmail.com>
2019-07-03 12:12:47 +0200

    Ajout du container

--- src/components/prime/Prime.js
+++ src/components/prime/Prime.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Header, Button, Form } from "semantic-ui-react";
+import { Container, Header, Button, Form, Grid } from "semantic-ui-react";
 import { findThePrime } from "../../utils/helpers";
 
 class Prime extends Component {
@@ -35,36 +35,41 @@ class Prime extends Component {
   sumbmitNumber() {
     const { number, history } = this.state;
     const thePrime = findThePrime(number);
-    this.setState({ prime: thePrime });
-    this.setState({ history: [...history, number] });
+    this.setState({ prime: thePrime, history: [...history, number] });
   }
 
   render() {
     const { number, prime, history } = this.state;
     return (
-      <div>
-        <Header as="h2" className="title">
-          Find the prime number
-        </Header>
-        <Form onSubmit={this.sumbmitNumber}>
-          <Form.Field width={4}>
-            <input
-              placeholder="Number"
-              name="number"
-              control="input"
-              type="number"
-              value={number}
-              onChange={this.updateField}
-            />
-          </Form.Field>
-          <Button type="submit">Submit</Button>
-        </Form>
-        {prime}
-        <Header as="h2" className="title">
-          Search history
-        </Header>
-        {history.length > 0 ? history.join(", ") : ""}
-      </div>
+      <Container>
+        <Grid columns={1}>
+          <Grid.Row centered>
+            <Grid.Column>
+              <Header as="h2" className="title">
+                Find the prime number
+              </Header>
+              <Form onSubmit={this.sumbmitNumber}>
+                <Form.Field width={4}>
+                  <input
+                    placeholder="Number"
+                    name="number"
+                    control="input"
+                    type="number"
+                    value={number}
+                    onChange={this.updateField}
+                  />
+                </Form.Field>
+                <Button type="submit">Submit</Button>
+              </Form>
+              {prime}
+              <Header as="h3" className="title">
+                Search history
+              </Header>
+              {history.length ? history.join(", ") : ""}
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </Container>
     );
   }
 }
